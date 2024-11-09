26bf270394615913ecf7e703062eaa9243c3e0c8
TiramisuOwl <97919959+TiramisuOwl@users.noreply.github.com>
2024-11-09 16:45:19 -0500

    It plays audio now :)

--- sound.js
+++ sound.js
@@ -1,1 +1,39 @@
 //script.js
+
+//Get references to all audio elements and checkboxes
+const audioElements = [
+    document.getElementById('birds'),
+    document.getElementById('trees'),
+    document.getElementById('stream'),
+    document.getElementById('frog'),
+];
+const checkboxes = document.querySelectorAll('[data-track]');
+
+//Play selected audio tracks + wimd
+document.getElementById('play-selected').addEventListener('click', () => {
+    if (wind.paused) {
+        wind.play();
+    }
+
+    //Play selected
+    audioElements.forEach((audio, index) => {
+        if (checkboxes[index].checked) {
+            audio.play();
+        } else {
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    });
+});
+
+document.getElementById('stop-all').addEventListener('click', ()  => {
+    wind.pause();
+    wind.currentTime = 0;
+
+    //Stop selected
+    audioElements.forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+    checkboxes.forEach(checkbox => checkbox.checked = false);
+});
